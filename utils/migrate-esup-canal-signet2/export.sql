465bb119a98395c8ed54334d4a07ac1ed2fef456
Pascal Rigaux <Pascal.Rigaux@univ-paris1.fr>
2017-03-10 17:27:02 +0100

    use esup-restdb (some more)

--- utils/migrate-esup-canal-signet2/export.sql
+++ utils/migrate-esup-canal-signet2/export.sql
@@ -1,1 +1,1 @@
-SELECT LOGIN_BM as user, URL_SIG as link, LIB_BM as name, DESC_BM as description, lib_parent as folders FROM sig_bookmark where TYPE_BM = 'book';
+SELECT LOGIN_BM as uid, URL_SIG as link, LIB_BM as name, DESC_BM as description, lib_parent as folders FROM sig_bookmark where TYPE_BM = 'book';
